522dce6bfada0b7a0525402296b2f328f1d37171
Fogus <mefogus@gmail.com>
2020-06-11 12:42:56 -0400

    project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject fogus/thneed "0.0.1-SNAPSHOT"
+  :description "One billion useful Clojure functions."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]])
+
