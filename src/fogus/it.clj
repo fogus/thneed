389096940a28c17cdecec1e89b182da10298161e
Fogus <mefogus@gmail.com>
2020-06-11 12:52:14 -0400

    info theory

---
+++ src/fogus/it.clj
@@ -0,0 +1,18 @@
+(ns fogus.it
+  "Utilities and functions pertaining to Information Theory.")
+
+
+(defn entropy
+  "Calculate the information entropy (Shannon entropy) of a
+  given input string."
+  [s]
+  (let [len (count s)]
+    (->> (frequencies s)
+         (map (fn [[_ v]]
+                (let [rf (/ v len)]
+                  (-> (Math/log rf)
+                      (/ (Math/log 2.0))
+                      (* rf)
+                      Math/abs))))
+         (reduce +))))
+
