92e4251a54f96ca93dae0b0a938859541515926f
Fogus <mefogus@gmail.com>
2020-07-16 12:43:34 -0400

    mapo

--- src/fogus/logic.clj
+++ src/fogus/logic.clj
@@ -1,4 +1,12 @@
 (ns fogus.logic
   (:require [clojure.core.logic :as °]))
 
+(defn mapo [fun coll res]
+  (°/conda
+    [(°/emptyo coll) (°/emptyo res)]
+    [(°/fresh [v r tail tail-res]
+       (°/conso v tail coll)
+       (°/conso r tail-res res)
+       (fun v r)
+       (mapo fun tail tail-res))]))
 
