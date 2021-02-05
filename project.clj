0d791418b7b20a1249c52c925eac0f1254756eff
Fogus <mefogus@gmail.com>
2021-02-05 13:29:15 -0500

    Porting from lein to deps.edn + clj

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject fogus/thneed "0.0.1-SNAPSHOT"
-  :description "One billion useful Clojure functions."
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure    "1.10.1"]
-                 [org.clojure/core.logic "1.0.0"]]
-  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]])
-
