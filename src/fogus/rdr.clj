745c74f6c9ede0a5b179205d30940f352d3d5700
Fogus <mefogus@gmail.com>
2022-03-08 13:51:43 -0500

    docstrings

--- src/fogus/rdr.clj
+++ src/fogus/rdr.clj
@@ -4,10 +4,10 @@
   (:import java.io.PushbackReader
            clojure.lang.LispReader))
 
-;; TODO: last branch is coerce which then fails if wrong types
 ;; TODO: varargs as?
 ;; TODO: primitive arrays
 ;; TODO: hint return of constructor functions?
+;; TODO: error when class || method not resolved
 ;; TODO: cache
 
 (set! *warn-on-reflection* true)
@@ -36,7 +36,7 @@
      (when method (symbol method))]))
 
 (defn qmethod-rdr
-  "Passthrough to the read handler for the qualified method handler. Calls through
+  "Pass-through to the read handler for the qualified method handler. Calls through
   the Var for development purposes."
   [rdr dot opts pending]
     (let [form (LispReader/read rdr true nil false opts)]
@@ -46,8 +46,6 @@
       (when (not (and klass-sym method-sym))
         (throw (RuntimeException. (str "expecting a qualified symbol for reader form #. got #." form " instead"))))
       `(make-fn ~klass-sym ~method-sym))))
-
-;; REFLECTION STUFF
 
 (defn get-field
   "Access to private or protected field.  field-name is a symbol or
@@ -68,16 +66,27 @@
           (int \.)
           rdr)))
 
+;; TREE BUILDING
+
 (def ranks '{long 1
              double 2
              int 3
              float 4})
 
-(defn- tcompare [[t1 _] [t2 _]]
+(defn- tcompare
+  "Comaparator for the type/arg tuple comprising the dispatch tree. Compares the
+  priority ranking as found in ranks or defaults to < if not found to avoid clashes
+  on types not found in ranks."
+  [[t1 _] [t2 _]]
   (compare (get ranks t1 0)
            (get ranks t2 1)))
 
 (defn- build-dispatch-tree
+  "Given a set of type signatures and an arglist, builds a tree representing the
+  type+arg matching for a set of signatures for a single arity. The tree is built
+  from nested maps sorted by the type priority rankings. Each branch represents a
+  single siganture and layers correspond to the same positional argument in all
+  of the signatures."
   [sigs args]
   (let [tuples (map #(partition 2 (interleave %1 %2)) sigs (cycle [args]))]
     (apply (fn m [& all] (apply merge-with m all))
@@ -87,13 +96,20 @@
                     (sorted-map-by tcompare)))
                 tuples))))
 
+;; CALLSITE BUILDING
+
 (def coercions
   '{int int
     float float
     long long
     double double})
 
-(defn- build-resolutions [types args]
+(defn- build-resolutions
+  "Given a seq of types and their corresponding arguments, builds a seq of coercion
+  forms for each argument. Attempts to look up a coercion function name in coercions
+  and builds a form for a call to that function with the corresponding arg, else
+  attached :tag metadata to that argument for the type."
+  [types args]
   (map (fn [t a]
          (let [coercer (get coercions t)]
            (if coercer
@@ -102,7 +118,12 @@
        types
        args))
 
-(defn build-callsite [types args target class-sym method-sym]
+(defn build-callsite
+  "Given the information required to build a call form, returns one of: a static
+  method call, an instance method call with target object name and hinting, or
+  a call to new if a constructor is signified. All arguments to the call form will
+  be coerced to the expected types."
+  [types args target class-sym method-sym]
   (cond (ctor? class-sym method-sym) `(new ~class-sym ~@(build-resolutions types args))
         target `(. ~(with-meta target {:tag class-sym}) ~method-sym ~@(build-resolutions types args))
         :default `(. ~class-sym ~method-sym ~@(build-resolutions types args))))
@@ -112,7 +133,14 @@
               float Float
               double Double})
 
+;; DISPATCH TABLE BUILDING
+
 (defn- build-conditional-dispatch
+  "Given a dispatch tree for a single arity, a stack of types encountered so far, the
+  arg list, and the data needed to build a call form, returns a dispatch table that checks
+  each possible argument type and the overloads of each that bottoms out into a call
+  that coerces its arguments to the expected types. The dispatch table is implemented as
+  nested conds that branch on the disparate types for each argument."
   [tree type-stack args target class-sym method-sym]
   (when tree
     `(cond
@@ -133,12 +161,24 @@
                default-branch (->> branches last second (vector :default))]
            (apply concat (conj (vec (butlast branches)) default-branch))))))
 
-(defn- build-simple-dispatch [args target class-sym method-sym]
+;; FUNCTION BUILDING
+
+(defn- build-simple-dispatch
+  "Given the information required to build a call form, returns one of: a static
+  method call, an instance method call with target object name and hinting, or
+  a call to new if a constructor is signified. None of the arguments are coerced."
+  [args target class-sym method-sym]
   (cond (ctor? class-sym method-sym) `(new ~class-sym ~@args)
         target `(. ~(with-meta target {:tag class-sym}) ~method-sym ~@args)
         :default `(. ~class-sym ~method-sym ~@args)))
 
-(defn- build-body [arity sigs static? class-sym method-sym]
+(defn- build-body
+  "Takes an arity count, a seq of signatures for it, a flag if the underlying method
+  is static, and the class and function names and uilds a function body for that arity.
+  If there are no overloads for this arity then the body has no conditional branching on
+  types and none of the arguments to the call are coerced. In the case of type overloading
+  a conditional dispatch table is built and arguments to calls are coerced as needed."
+  [arity sigs static? class-sym method-sym]
   (let [arglist  (repeatedly arity gensym)
         target   (when (not static?) (with-meta (gensym "self") {:tag class-sym}))
         dispatch-tree (build-dispatch-tree sigs arglist)]
@@ -154,12 +194,21 @@
               (build-body arity sigs static? class-sym method-sym))
             arities)))
 
+;; MACRO SUPPORT
+
 (defn- overloads
   "Returns a seq of the overides for a given method in clojure.reflect/reflect structs."
   [details method-sym]
   (->> details :members (filter (comp #{method-sym} :name))))
 
 (defn- build-method-descriptor
+  "Takes a class and method symbol and builds a descriptor for the method containing:
+
+  :static? - a flag indicating if the method is static
+  :klass - the resolved class
+  :class-sym - the symbolic class name
+  :method-sym - the symbolic method name
+  :arities - a map of arity-count => signatures (parameter types and return type)"
   [klass-sym method-sym]
   (let [form    (symbol (name klass-sym) (name method-sym))
         klass   (resolve klass-sym)
@@ -180,10 +229,18 @@
      :klass      klass
      :arities    arities}))
 
-(defmacro make-fn [class-sym method-sym]
+(defmacro make-fn
+  "Given a class and method name, returns a function that dispatches to a call of 
+  the method with its arguments coerced to the expected types. If the method-sym argument
+  is the same as the class-sym argument then the function returned dispatches to that
+  class's constructor with its arguments coerced. In the case where an unsupported type is
+  passed to the generated function, a coersion exception will occur."
+  [class-sym method-sym]
   (let [descr (build-method-descriptor class-sym method-sym)]
     `(let []
        ~(build-method-fn descr))))
+
+
 
 (comment
   (attach-qmethod-reader! qmethod-rdr)
